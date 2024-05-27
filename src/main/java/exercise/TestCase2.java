026816429ff3f719c675d61bac4e6469e59d8a2d
Shashankvjogi <shashankjogi111@gmail.com>
2024-05-27 09:54:35 +0530

    Test Case 2 and 3 are added

---
+++ src/main/java/exercise/TestCase2.java
@@ -0,0 +1,60 @@
+package exercise;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TestCase2 {
+    public static void main(String[] args) {
+        String username ="testing11234";
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("http://automationexercise.com");
+
+        String ExpectedTitle = "Automation Exercise";
+        String ActualTitle = driver.getTitle();
+        if(ExpectedTitle.equalsIgnoreCase(ActualTitle)){
+            System.out.println("Home Page is Visible succesffuly");
+        }
+        else {
+            System.out.println("Home Page is Not Visible");
+        }
+        driver.findElement(By.xpath("//a[text()=' Signup / Login']")).click();
+
+        boolean verify = driver.findElement(By.xpath("//h2[text()='Login to your account']")).isDisplayed();
+        if(verify){
+            System.out.println("Login to your account is Displayed");
+        }
+        else {
+            System.out.println("Login to your account is not Displayed");
+        }
+
+        driver.findElement(By.xpath("//input[@data-qa='login-email']")).sendKeys("testing11234@gmail.com");
+        driver.findElement(By.xpath("//input[@data-qa='login-password']")).sendKeys("testing");
+        driver.findElement(By.xpath("//button[@data-qa='login-button']")).click();
+
+        boolean verify3 = driver.findElement(By.xpath("//a[text()=' Logged in as ']//child::b[text()='"+username+"']")).isDisplayed();
+        if(verify3){
+            System.out.println("Logged in as username is Displayed");
+        }
+        else {
+            System.out.println("Logged in as username is not Displayed");
+        }
+
+        driver.findElement(By.xpath("//a[text()=' Delete Account']")).click();
+
+        boolean verify4 = driver.findElement(By.xpath("//b[text()='Account Deleted!']")).isDisplayed();
+        if(verify4){
+            System.out.println("Account Deleted! is Displayed");
+        }
+        else {
+            System.out.println("Account Deleted! is not Displayed");
+        }
+        driver.findElement(By.xpath("//a[@data-qa='continue-button']")).click();
+
+        driver.close();
+    }
+}
---
+++ src/main/java/exercise/TestCase3.java
@@ -0,0 +1,48 @@
+package exercise;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TestCase3 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("http://automationexercise.com");
+
+        String ExpectedTitle = "Automation Exercise";
+        String ActualTitle = driver.getTitle();
+        if(ExpectedTitle.equalsIgnoreCase(ActualTitle)){
+            System.out.println("Home Page is Visible succesffuly");
+        }
+        else {
+            System.out.println("Home Page is Not Visible");
+        }
+
+        driver.findElement(By.xpath("//a[text()=' Signup / Login']")).click();
+
+        boolean verify = driver.findElement(By.xpath("//h2[text()='Login to your account']")).isDisplayed();
+        if(verify){
+            System.out.println("Login to your account is Displayed");
+        }
+        else {
+            System.out.println("Login to your account is not Displayed");
+        }
+
+        driver.findElement(By.xpath("//input[@data-qa='login-email']")).sendKeys("incorrect@gmail.com");
+        driver.findElement(By.xpath("//input[@data-qa='login-password']")).sendKeys("incorrect");
+        driver.findElement(By.xpath("//button[@data-qa='login-button']")).click();
+
+        boolean verify3 = driver.findElement(By.xpath("//p[text()='Your email or password is incorrect!']")).isDisplayed();
+        if(verify3){
+            System.out.println("Your email or password is incorrect! is Displayed");
+        }
+        else {
+            System.out.println("Your email or password is incorrect! is not Displayed");
+        }
+
+        driver.close();
+    }
+}
